40174a3b3f454ac8dd0aa6c028a699588e59882c
waherrera1 <waherrera1@miners.utep.edu>
2025-04-09 23:49:54 -0600

    code
    code

--- code_7.py
+++ code_7.py
@@ -4,33 +4,74 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 filename = 'Flight_Delays_2018.csv'
 
 #CODE
-
 df = pd.read_csv(filename)
-df.describe()
 
-#creating a heatmap to find which numerical variables are correlated the most with the flight delays variable
-#which reveals the variables with the highest correlation to delays are departure delay and carrier delay
+
+#DESCRIPTIVE STATISTICS
+#creating a heatmap to find which variables have the highest correlation with delays initially
 corr = df.corr(numeric_only = True).round(2)
 sns.heatmap(corr, annot= True, vmax = 1, vmin = -0.5, cmap = 'icefire')
-
-#Getting the descriptive statistics and creating a scatter plot to compare airports based on flight delays
-airlines = df.groupby('OP_CARRIER_NAME')['ARR_DELAY']
-airlines.describe()
-
-df.plot.scatter(x='OP_CARRIER_NAME', y='ARR_DELAY')
 plt.show()
 
-#Creating a scatter plot to compare airlines based on flight delays
-airlines = df.groupby('ORIGIN')['ARR_DELAY']
-airlines.describe()
 
-df.plot.scatter(x='ORIGIN', y='ARR_DELAY')
+#Getting the descriptive statistics for the arrival delays 
+#to compare with the statistics of arrival delays by airline and by airport to reduce the scope of data to analyze
+print()
+print("---Descriptive statistics for arrival delays---")
+print(df['ARR_DELAY'].describe().round(2))
+
+
+print()
+print("---Descriptive statistics for arrival delays based on airline---")
+print(df.groupby('OP_CARRIER_NAME')['ARR_DELAY'].agg(['mean','min','max']).sort_values(by ='mean', ascending=False).round(2))
+
+
+print()
+print("---Descriptive statistics for arrival delays based on airports---")
+print(df.groupby('DEST')['ARR_DELAY'].agg(['mean','min','max']).sort_values(by='mean', ascending=False).head(10).round(2))
+
+
+#Based on these results, I will reduce my scope by only including the 3 airports and 3 airlines with the highest delays
+reduced = df.query("DEST=='ECP' or DEST =='SUN' or DEST == 'SHD'")
+smol_df = df.query("OP_CARRIER_NAME=='Frontier Airlines Inc.' or OP_CARRIER_NAME=='JetBlue Airways' or OP_CARRIER_NAME=='Spirit Air Lines'")
+
+#Creating an update correlation heatmap to see highest correlations with arrival delays after the reduction of the data
+corr2 = reduced.corr(numeric_only = True).round(2)
+sns.heatmap(corr2, annot= True, vmax = 1, vmin = -0.5, cmap = 'icefire')
 plt.show()
 
-#Based on these results, I will remove the airlines and airports that experience the least delays to focus on data that is more significant to delays
+corr3 = smol_df.corr(numeric_only = True).round(2)
+sns.heatmap(corr3, annot= True, vmax = 1, vmin = -0.5, cmap = 'icefire')
+plt.show()
 
 
+#PREDICTIVE ANALYTICS, linear regression model and OLS
+
+#Based on the results above, the predictor variables I selected are DEP_DELAY, CARRIER_DELAY & LATE_AIRCRAFT_DELAY
+#None of the variables tested could provide a good model for predicting flight delays
+
+x = df['DEP_DELAY']
+y = df['ARR_DELAY']
+x = sm.add_constant(x)
+regression_model = sm.OLS(y,x).fit()
+print(regression_model.summary())
+
+a,b = np.polyfit(df['DEP_DELAY'],df['ARR_DELAY'],1)
+plt.scatter(df['DEP_DELAY'],df['ARR_DELAY'], color='black')
+plt.plot(df['DEP_DELAY'], a*df['ARR_DELAY']+b)
+plt.text(250,1750, 'y = ' + '{:.3f}'.format(b) + ' + {:.3f}'.format(a) + 'x', size=12)
+plt.xlabel('Departure Delay')
+plt.ylabel('Arrival Delay')
+plt.xlim(0,2000)
+plt.ylim(0,2000)
+plt.show()
+
+# Sources
+# OpenAI. (2025), Prompt: “How can I sort values from greatest to least when printing something from a df”. ChatGPT.
+
+# OpenAI. (2025), Prompt: “How can I edit the x and y range in a scatter plot in python”. ChatGPT.
